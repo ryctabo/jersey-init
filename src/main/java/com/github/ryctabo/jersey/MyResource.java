49bdfa8772f9cef6601a3b3375a8c4bc6cf80267
Gustavo Pacheco <ryctabo@gmail.com>
2018-01-21 23:25:50 -0500

    Add license

--- src/main/java/com/github/ryctabo/jersey/MyResource.java
+++ src/main/java/com/github/ryctabo/jersey/MyResource.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Gustavo Pacheco.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.ryctabo.jersey;
 
 import javax.ws.rs.GET;
@@ -9,7 +25,7 @@ import javax.ws.rs.core.MediaType;
  * Root resource (exposed at "myresource" path).
  *
  * @author Gustavo Pacheco (ryctabo at gmail.com)
- * @version 1.0-SNAPSHOT
+ * @version 1.0
  */
 @Path("myresource")
 public class MyResource {
